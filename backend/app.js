fa1eab05110f25ec08029354334e2857178fbb7b
Antti Niiles <antti.niiles@gmail.com>
2021-11-04 10:28:56 +0200

    cleaning up

--- backend/app.js
+++ backend/app.js
@@ -8,7 +8,6 @@ puppeteer.use(StealthPlugin())
 
 var express = require("express");
 var app = express();
-app.use('/public', express.static(__dirname + '/public'));
 app.listen(3000, () => {
  console.log("Server running on port 3000");
 });
@@ -21,17 +20,6 @@ app.get("/members/:clubId", async function(req, res, next) {
     var browser = await puppeteer.launch({ headless: true });    
     var page = await browser.newPage();
     await page.setViewport(viewPort)
-    const options = {
-        path: 'public/screenshot.png',  // set's the name of the output image'
-        fullPage: false,
-        // dimension to capture
-        clip: {      
-            x: 0,   // x coordinate
-            y: 650,   // y coordinate
-            width: 1280,      // width
-            height: 2120   // height
-        }
-    }        
     page.on('response', async (response) => { 
         if (response.url().startsWith('https://proclubs.ea.com/api/nhl/members/career/stats')) {
             console.log(response.url())
@@ -77,7 +65,7 @@ app.get("/team/:name", async function(req, res, next) {
             res.json(data);    
         }   
     }); 
-    await page.goto('https://www.ea.com/fi-fi/games/nhl/nhl-22/pro-clubs/rankings#platform=ps4', {
+    await page.goto('https://www.ea.com/fi-fi/games/nhl/nhl-22/pro-clubs/rankings#platform=' + process.env.PLATFORM, {
         waitUntil: 'networkidle2'
     });
     await page.waitForSelector('shadow/#search');
