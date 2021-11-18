a00b37570bd9b930e617d7fcddeed50c744af025
Antti Niiles <antti.niiles@gmail.com>
2021-11-18 21:54:44 +0200

    make tests folder

--- bot/extra/fb.py
+++ bot/extra/fb.py
@@ -2,14 +2,13 @@ import firebase_admin
 import os
 from firebase_admin import credentials
 from firebase_admin import firestore
-import data_service
 from dotenv import load_dotenv
 load_dotenv('../.env')
 CLUB_ID = os.getenv('CLUB_ID')
 
 
 # Use a service account
-cred = credentials.Certificate('./firebase-credentials.json')
+cred = credentials.Certificate('../firebase-credentials.json')
 firebase_admin.initialize_app(cred)
 
 db = firestore.client()
@@ -34,10 +33,3 @@ def find_match_by_id(matchId):
         match = doc.to_dict()
         matches.append(match)
     return matches
-
-if __name__ == '__main__':
-    matches = find_matches_by_club_id("12172")
-    for match in matches:
-        print(data_service.format_result(match))
-    match = find_match_by_id("904207420418")
-    print(data_service.format_result(match))
