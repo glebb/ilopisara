44990d8acafbd77c19eefd31c2e4597e043d7950
Antti <antti.niiles@gmail.com>
2022-10-09 20:12:09 +0300

    allow tracking multiple teams

--- bot/db_mongo.py
+++ bot/db_mongo.py
@@ -1,4 +1,6 @@
 import asyncio
+import getopt
+import sys
 
 import db_utils
 import helpers
@@ -29,20 +31,25 @@ async def watch(result_handler):
                     await result_handler((insert_change["fullDocument"]))
 
 
-async def update_matches():
+async def update_matches(club_id, platform):
     for game_type in helpers.GAMETYPE:
-        matches = api.get_matches(count=50, game_type=game_type.value)
+        postfix = ""
+        if club_id != helpers.CLUB_ID:
+            postfix = "_" + club_id + "_" + platform
+        matches = api.get_matches(
+            club_id=club_id, platform=platform, count=10, game_type=game_type.value
+        )
         for match in matches:
-            await db.replica.update_one(
+            await db["replica" + postfix].update_one(
                 {"matchId": match["matchId"]}, {"$setOnInsert": match}, upsert=True
             )
             enriched_match = db_utils.enrich_match(match, game_type)
-            await db.matches.update_one(
+            await db["matches" + postfix].update_one(
                 {"matchId": match["matchId"]},
                 {"$setOnInsert": enriched_match},
                 upsert=True,
             )
-            await db.opponents.update_one(
+            await db["opponents" + postfix].update_one(
                 {"name": enriched_match["opponent"]["name"]},
                 {"$setOnInsert": enriched_match["opponent"]},
                 upsert=True,
@@ -82,4 +89,15 @@ async def find_matches_for_player(player_id):
 
 
 if __name__ == "__main__":
-    asyncio.run(update_matches())
+    club_id = helpers.CLUB_ID
+    platform = helpers.PLATFORM
+    try:
+        opts, args = getopt.getopt(sys.argv[1:], "p:c:", ["platform=", "clubid="])
+        for opt, arg in opts:
+            if opt == "--platform":
+                platform = arg
+            elif opt == "--clubid":
+                club_id = arg
+    except getopt.GetoptError as e:
+        print(e)
+    asyncio.run(update_matches(club_id, platform))
