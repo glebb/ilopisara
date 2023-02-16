19526486393aa86f8c9287cbfe8a1808de907fa5
Antti <antti.niiles@gmail.com>
2023-02-16 20:11:11 +0200

    test nag

--- bot/bot.py
+++ bot/bot.py
@@ -78,12 +78,9 @@ class Bot(commands.Bot):
     @tasks.loop(minutes=10)
     async def check_latest_game(self):
         await self.wait_until_ready()
-        if not self.now:
-            self.now = datetime.datetime.now().timestamp()
-            return
         if not (19 <= datetime.datetime.now().hour < 22):
             return
-        if self.now + 3600 * 36 > datetime.datetime.now().timestamp():
+        if self.now and self.now + 3600 * 36 > datetime.datetime.now().timestamp():
             return
         channel = self.get_channel(int(helpers.DISCORD_CHANNEL))
         logger.info("Checking latest game timestamp")
@@ -94,6 +91,10 @@ class Bot(commands.Bot):
         latest_game_timestamp = int(
             from_dict(data_class=Match, data=latest_game[0]).timestamp
         )
+        if not self.now:
+            self.now = latest_game_timestamp
+            return
+
         if latest_game_timestamp + 3600 * 36 < datetime.datetime.now().timestamp():
             self.now = datetime.datetime.now().timestamp()
             await channel.send(
