955b1301fe22efe93f37eeb33edfc4da5c224e42
Antti <antti.niiles@gmail.com>
2023-03-25 16:30:27 +0200

    improve logging

--- bot/base_logger.py
+++ bot/base_logger.py
@@ -6,5 +6,9 @@ from dotenv import load_dotenv
 load_dotenv("../.env")
 DEBUG = int(os.getenv("DEBUG"))
 
-logging.basicConfig(level=logging.INFO if DEBUG else logging.WARNING)
+logging.basicConfig(
+    level=logging.INFO if DEBUG else logging.WARNING,
+    format="%(asctime)s %(levelname)-8s %(message)s",
+    datefmt="%Y-%m-%d %H:%M:%S",
+)
 logger = logging
