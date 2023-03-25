c06eeb48be483dc9ecc2a984e31b3a05285c4ea5
Antti <antti.niiles@gmail.com>
2023-03-25 20:40:53 +0200

    format file log

--- bot/base_logger.py
+++ bot/base_logger.py
@@ -5,14 +5,16 @@ from dotenv import load_dotenv
 
 load_dotenv("../.env")
 DEBUG = int(os.getenv("DEBUG"))
-
+rootLogger = logging.getLogger()
+logFormatter = logging.Formatter("%(asctime)s %(levelname)-8s %(message)s")
 logging.basicConfig(
     level=logging.INFO if DEBUG else logging.WARNING,
     format="%(asctime)s %(levelname)-8s %(message)s",
     datefmt="%Y-%m-%d %H:%M:%S",
 )
-rootLogger = logging.getLogger()
+
 fileHandler = logging.FileHandler("{0}/{1}.log".format("./", "log"))
+fileHandler.setFormatter(logFormatter)
 rootLogger.addHandler(fileHandler)
 
 logger = logging
