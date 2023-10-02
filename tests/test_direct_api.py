e18290bc6dde1161e4a68111655baf2ffbfc62dd
Antti <antti.niiles@gmail.com>
2023-10-02 22:35:47 +0300

    some tests

--- tests/test_direct_api.py
+++ tests/test_direct_api.py
@@ -1,21 +1,30 @@
 # pylint: disable=C0413
 import os
 import sys
-
-import pytest
+from typing import List, Literal
 
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+
+import pytest
+from dacite import from_dict
+
+from ilobot import db_utils
+from ilobot.base_logger import logger
 from ilobot.data import api
+from ilobot.helpers import GAMETYPE
+from ilobot.models import Match
 
 PLATFORM = "common-gen5"
 CLUB_ID = "2"
 
 
-@pytest.mark.parametrize("count", [(1), (1)])
-def test_get_matches(count):
-    matches = api.get_matches(CLUB_ID, PLATFORM, count)
+@pytest.mark.parametrize("count", [(1)])
+def test_get_matches(count: Literal[1]):
+    matches = api.get_matches(CLUB_ID, PLATFORM, count, GAMETYPE.REGULARSEASON.value)
     assert len(matches) == count
-    assert type(matches) == type([])
+    assert isinstance(matches, List) is True
+    match = db_utils.enrich_match(matches[0], GAMETYPE.REGULARSEASON)
+    from_dict(data_class=Match, data=match)
 
 
 def test_get_matches_max_result_count_capped_to_5():
@@ -24,48 +33,47 @@ def test_get_matches_max_result_count_capped_to_5():
 
 
 @pytest.mark.parametrize("clubId", [(""), ("00006660000"), ("cxvcxvcxvc"), None])
-def test_get_matches_wrong_id(clubId):
+def test_get_matches_wrong_id(clubId: Literal['', '00006660000', 'cxvcxvcxvc'] | None):
     matches = api.get_matches(clubId, PLATFORM)
     assert len(matches) == 0
-    assert type(matches) == type([])
+    assert isinstance(matches, List) is True
 
 
 def test_get_members():
     members = api.get_members(CLUB_ID, PLATFORM)
     assert len(members) > 1
-    assert type(members) == type([])
+    assert isinstance(members, List) is True
 
 
 @pytest.mark.parametrize("club_id", [(""), ("00006660000"), ("cxvcxvcxvc"), None])
-def test_get_members_wrong_id(club_id):
+def test_get_members_wrong_id(club_id: Literal['', '00006660000', 'cxvcxvcxvc'] | None):
     members = api.get_members(club_id, PLATFORM)
     assert len(members) == 0
-    assert type(members) == type([])
+    assert isinstance(members, List) is True
 
 
 def test_get_team_record():
     record = api.get_team_record("Nighthawks", PLATFORM)
     assert len(record) == 1
-    assert type(record) == type({})
+    assert isinstance(record, dict) is True
 
 
 def test_get_team_record_unknown_team():
     record = api.get_team_record("poiuerew slfdsj vmxcnvxc", PLATFORM)
     assert len(record) == 0
-    assert type(record) == type({})
+    assert isinstance(record, dict) is True
 
 
 def test_team_info():
     info = api.get_team_info(CLUB_ID, PLATFORM)
     assert len(info) == 1
-    assert type(info) == type({})
+    assert isinstance(info, dict) is True
 
 
 def test_seasonal_stats():
     stats = api.get_seasonal_stats(CLUB_ID, PLATFORM)
-    print(stats)
     assert len(stats) == 1
-    assert type(stats) == type([])
+    assert isinstance(stats, List) is True
 
 
 def test_get_member():
