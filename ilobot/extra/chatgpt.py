e37fe9d79ef889d01b1bcfc580a06d97bdf8bd8b
Antti <antti.niiles@gmail.com>
2023-10-10 19:29:00 +0300

    chat tuning

--- ilobot/extra/chatgpt.py
+++ ilobot/extra/chatgpt.py
@@ -40,7 +40,6 @@ skip_keys = [
     "playerLevel",
     "skfow",
     "skfol",
-    
 ]
 
 skip_just_player_keys = [
@@ -49,7 +48,6 @@ skip_just_player_keys = [
     "score",
     "scoreRaw",
     "scoreString",
-    
 ]
 
 # Fixed mappings for key name conversions
@@ -58,7 +56,7 @@ key_mappings = {
     "opponentTeamArtAbbr": "Opponent Team abbreviation",
     "teamArtAbbr": "Team abbreviation",
     "name": "Club name",
-    "ppo": "Power-play opportunities"
+    "ppo": "Power-play opportunities",
 }
 
 
@@ -83,7 +81,7 @@ def convert_keys(temp):
         if "position" in temp:
             if key in skip_just_player_keys:
                 continue
-        
+
         if isinstance(value, dict):
             # Recursively process nested dictionaries
             converted_data[xxx] = convert_keys(value)
@@ -97,15 +95,16 @@ def convert_keys(temp):
             converted_data[key] = value
     return converted_data
 
+
 def clean_up_data(game: dict):
     converted_data = convert_keys(game)
     for key in converted_data["clubs"].keys():
         if key == str(CLUB_ID):
             converted_data["clubs"][key]["players"] = converted_data["players"][key]
         else:
-            converted_data["clubs"][key]["players"] = {} 
-    
-    clubs = {}    
+            converted_data["clubs"][key]["players"] = {}
+
+    clubs = {}
     for club_id, club_data in converted_data["clubs"].items():
         club_name = club_data["details"]["Club name"]
         clubs[club_name] = club_data
@@ -114,17 +113,21 @@ def clean_up_data(game: dict):
         clubs[club_name]["players"] = {
             player_data["playername"]: player_data
             for player_id, player_data in club_data["players"].items()
-        
-    }
+        }
     del converted_data["players"]
-    converted_data['clubs'] = clubs                     
+    converted_data["clubs"] = clubs
     return converted_data
+
 
 def check_dnf(game: dict):
     for club in game["clubs"]:
-        if game["clubs"][club]["winnerByDnf"] != "0" or game["clubs"][club]["winnerByGoalieDnf"] != "0":
+        if (
+            game["clubs"][club]["winnerByDnf"] != "0"
+            or game["clubs"][club]["winnerByGoalieDnf"] != "0"
+        ):
             return True
     return False
+
 
 async def write_gpt_summary(game: dict, history=None):
     our_team = game["clubs"][CLUB_ID]["details"]["name"]
@@ -133,31 +136,52 @@ async def write_gpt_summary(game: dict, history=None):
     messages = [
         {
             "role": "system",
-            "content": f"You are a Yoosef, general manager of hockey club {our_team}. You are extremely critical and give praise only on most exceptional performances."
-            
+            "content": f"You are a Yoosef, general manager of hockey club {our_team}. You are extremely critical, point out the mistakes players make and you give praise only on most exceptional performances.",
         },
         {
             "role": "user",
-            "content": "Analyze the hockey game that just took place, based on following json data and imaginary events. You critique the performance of the team and its individual players and point out the mistakes they did. Address your players directly."
-        },        
+            "content": "Analyze the hockey game that just took place, based on following json data and imaginary events. Critique the performance of the team and its individual players.",
+        },
     ]
-    if  game["clubs"][CLUB_ID]["losses"] != "0" and  int(game["clubs"][CLUB_ID]["shots"]) < 20:
-        messages.append({"role": "user", "content": "Comment the poor shots statistics with a phrase 'VETOJA HYVÄT HERRAT!'"})    
+    if (
+        game["clubs"][CLUB_ID]["losses"] != "0"
+        and int(game["clubs"][CLUB_ID]["shots"]) < 20
+    ):
+        messages.append(
+            {
+                "role": "user",
+                "content": "Comment the poor shots statistics with a phrase 'VETOJA HYVÄT HERRAT!'",
+            }
+        )
 
     messages.append({"role": "user", "content": "###\n" + json_output})
 
-        
     if check_dnf(cleaned_game):
-        messages.append({"role": "user", "content": "If the data indicates 'winnerByDnf' or 'winnerByGoalieDnf' with other than value 0, make a big deal about opponent chickening out by not finishing the game properly. Don't mention the data keys or values as such."})
-        messages.append({"role": "assistant", "content": "The opponent cowardly quit the game before it was finished."})
-        messages.append({"role": "assistant", "content": "The opponent demonstrated despicable attitude by quitting the match until it was finished."})
+        messages.append(
+            {
+                "role": "user",
+                "content": "If the data indicates 'winnerByDnf' or 'winnerByGoalieDnf' with other than value 0, make a big deal about opponent chickening out by not finishing the game properly. Don't mention the data keys or values as such.",
+            }
+        )
+        messages.append(
+            {
+                "role": "assistant",
+                "content": "The opponent cowardly quit the game before it was finished.",
+            }
+        )
+        messages.append(
+            {
+                "role": "assistant",
+                "content": "The opponent demonstrated despicable attitude by quitting the match until it was finished.",
+            }
+        )
     messages.append({"role": "user", "content": "Limit the text to 290 words."})
-    
+
     try:
         chat_completion = await openai.ChatCompletion.acreate(
-        model="gpt-3.5-turbo", messages=messages, temperature=0.9
-    )
+            model="gpt-3.5-turbo", messages=messages, temperature=0.9
+        )
     except ServiceUnavailableError:
         logger.exception("OPENAI error")
-        return None        
+        return None
     return "Yoosef's analysis\n" + chat_completion["choices"][0]["message"]["content"]
